70669deb35a9e005631c31274e68d494328e9dbd
Jacky Yan <jcky_yan1314@hotmail.com>
2017-04-11 22:42:05 +0800

    Sending Email Script

--- indexform_hdl2.php
+++ indexform_hdl2.php
@@ -2,10 +2,18 @@
 
 //error_reporting(E_ALL);
 $to = "lifemusic1314@gmail.com";
-$subject = "Test mail";
-$message = "Hello! This is a simple email message.";
-$from = "person2@gmail.com";
-$headers = "From:" . $from;
-mail($to,$subject,$message,$headers) or die("errors");
-echo "Mail Sent.";
+$subject = "Trustgate Enquiry";
+$message = 'Name: '. $_POST['name'] .'<br><br>'.'Email: '. $_POST['email'].'<br><br>'.'Contact Number: '. $_POST['hp'].'<br><br>'.'Company/Organisation: '. $_POST['companyname'].'<br><br>'.'Message: '.'<br>'. $_POST['message'];
+
+$from = $_POST['email'];
+$headers = "From:" . $_POST['email'];
+$send = mail($to,$subject,$message,$headers)
+
+if($send){
+    $message= 'Success::<div class="alert alert-success"><strong><i class="fa fa-info message-icon"></i>Message Sent Successfully.</strong></div>';
+}else{
+  echo 'Message could not be sent.';
+  $message=  'Error::<div class="alert alert-danger"><strong><i class="fa fa-info message-icon"></i>Something Went Wrong While Sending Message.</strong></div>';
+}
+echo $message;
 ?>
